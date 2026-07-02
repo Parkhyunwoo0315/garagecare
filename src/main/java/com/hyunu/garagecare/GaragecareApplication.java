13734d155bc3397e47068c7fad9f6dab1d2d97e9
Park HyunWoo <roy040315@gmail.com>
2026-07-03 02:25:23 +0900

    init: GarageCare spring Boot 프로젝트 생성

---
+++ src/main/java/com/hyunu/garagecare/GaragecareApplication.java
@@ -0,0 +1,13 @@
+package com.hyunu.garagecare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaragecareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaragecareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyunu/garagecare/GaragecareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyunu.garagecare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaragecareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
