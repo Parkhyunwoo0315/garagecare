85e95c6b2b70d3e800b838ceceb219e6022f2a61
Park HyunWoo <roy040315@gmail.com>
2026-08-05 23:50:31 +0900

    test(Member): 로그인 화면 조회 테스트

--- src/test/java/com/hyunu/garagecare/member/controller/MemberControllerTest.java
+++ src/test/java/com/hyunu/garagecare/member/controller/MemberControllerTest.java
@@ -8,8 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -24,5 +23,14 @@ class MemberControllerTest {
         mockMvc.perform(get("/members/signup"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("member/signup-form"));
+    }
+
+    @Test
+    @DisplayName("로그인 화면 조회")
+    void loginForm() throws Exception {
+        mockMvc.perform(get("/members/login"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("member/login-form"))
+                .andExpect(model().attributeExists("form"));
     }
 }
