afffb612baa2561f60d93b5ef5772b850d3b21a2
Park HyunWoo <roy040315@gmail.com>
2026-08-08 14:21:26 +0900

    feat(Reservation): 예약 도메인 및 저장소 구현

---
+++ src/main/java/com/hyunu/garagecare/reservation/domain/Reservation.java
@@ -0,0 +1,85 @@
+package com.hyunu.garagecare.reservation.domain;
+
+import com.hyunu.garagecare.member.domain.Member;
+import com.hyunu.garagecare.vehicle.domain.Vehicle;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "reservations")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "vehicle_id", nullable = false)
+    private Vehicle vehicle;
+
+    @Column(name = "reservation_date", nullable = false)
+    private LocalDate reservationDate;
+
+    @Column(name = "reservation_time", nullable = false)
+    private LocalTime reservationTime;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private ReservationStatus status;
+
+    @OneToMany(
+            mappedBy = "reservation",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private final List<ReservationItem> reservationItems = new ArrayList<>();
+
+    private Reservation(
+            Member member,
+            Vehicle vehicle,
+            LocalDate reservationDate,
+            LocalTime reservationTime
+    ) {
+        this.member = member;
+        this.vehicle = vehicle;
+        this.reservationDate = reservationDate;
+        this.reservationTime = reservationTime;
+        this.status = ReservationStatus.PENDING;
+    }
+
+    public static Reservation create(
+            Member member,
+            Vehicle vihicle,
+            LocalDate reservationDate,
+            LocalTime reservationTime
+    ) {
+        return new Reservation(
+                member,
+                vehicle,
+                reservationDate,
+                reservationTime
+        );
+    }
+
+    public void addReservationItem(ReservationItem reservationItem) {
+        reservationItems.add(reservationItem);
+        reservationItem.assignReservation(this);
+    }
+
+    public void cancel() {
+        this.status = ReservationStatus.CANCELED;
+    }
+}
---
+++ src/main/java/com/hyunu/garagecare/reservation/domain/ReservationItem.java
@@ -0,0 +1,55 @@
+package com.hyunu.garagecare.reservation.domain;
+
+import com.hyunu.garagecare.maintenance.domain.MaintenanceItem;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(
+        name = "reservation_items",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_reservation_maintenance_item",
+                        columnNames = {
+                                "reservation_id",
+                                "maintenance_item_id"
+                        }
+                )
+        }
+)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ReservationItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(
+            name = "reservation-id",
+            nullable = false
+    )
+    private Reservation reservation;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(
+            name = "maintenance_item_id",
+            nullable = false
+    )
+    private MaintenanceItem maintenanceItem;
+
+    private ReservationItem(MaintenanceItem maintenanceItem) {
+        this.maintenanceItem = maintenanceItem;
+    }
+
+    public static ReservationItem create(MaintenanceItem maintenanceItem) {
+        return new ReservationItem(maintenanceItem);
+    }
+
+    void assignReservation(Reservation reservation) {
+        this.reservation = reservation;
+    }
+}
