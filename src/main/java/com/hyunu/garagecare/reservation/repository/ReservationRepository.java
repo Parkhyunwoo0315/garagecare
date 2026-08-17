0b43ed85418c54e2e6d28add6dfa13a082166727
Park HyunWoo <roy040315@gmail.com>
2026-08-17 13:01:52 +0900

    feat(Reservation): 예약 목록 및 상세 조회 서비스 구현

--- src/main/java/com/hyunu/garagecare/reservation/repository/ReservationRepository.java
+++ src/main/java/com/hyunu/garagecare/reservation/repository/ReservationRepository.java
@@ -3,8 +3,11 @@ package com.hyunu.garagecare.reservation.repository;
 import com.hyunu.garagecare.reservation.domain.Reservation;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ReservationRepository
         extends JpaRepository<Reservation, Long>{
@@ -13,5 +16,19 @@ public interface ReservationRepository
     List<Reservation>
     findAllByMemberIdOrderByReservationDateDescReservationTimeDesc(
             Long memberId
+    );
+
+    @Query("""
+            select distinct r
+            from Reservation r
+            join fetch r.member
+            join fetch r.vehicle
+            left join fetch r.reservationItems ri
+            left join fetch ri.maintenanceItem
+            where r.id = :reservationId
+            """)
+    Optional<Reservation> findDetailById(
+            @Param("reservationId")
+            Long reservationId
     );
 }
