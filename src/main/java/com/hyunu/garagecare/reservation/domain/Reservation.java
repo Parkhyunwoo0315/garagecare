ec6be3e556a7a0cf2c25d2141ae21092bc98edd3
Park HyunWoo <roy040315@gmail.com>
2026-08-28 01:42:43 +0900

    perf(Database): 예약 조회 인덱스 실행계획 분석

--- src/main/java/com/hyunu/garagecare/reservation/domain/Reservation.java
+++ src/main/java/com/hyunu/garagecare/reservation/domain/Reservation.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "reservations")
+@Table
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Reservation {
