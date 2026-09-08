327dcff1ae672e3afac306de6420387e620f2604
Park HyunWoo <roy040315@gmail.com>
2026-09-08 19:46:07 +0900

    test(CI): CI 실패 동작 검증

--- src/test/java/com/hyunu/garagecare/reservation/repository/ReservationRepositoryTest.java
+++ src/test/java/com/hyunu/garagecare/reservation/repository/ReservationRepositoryTest.java
@@ -289,7 +289,7 @@ public class ReservationRepositoryTest {
                 );
 
         // then
-        assertThat(result.isEmpty()).isTrue();
+        assertThat(result.isEmpty()).isFalse();
         assertThat(result.getTotalElements()).isZero();
         assertThat(result.getTotalPages()).isZero();
     }
